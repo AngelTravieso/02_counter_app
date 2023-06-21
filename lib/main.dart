895385ca4642fb6c0b9c684ffd2fd141296b8e39
atravieso <angeltraviesoc@gmail.com>
2023-06-20 22:07:12 -0400

    Scaffold y buenas practicas

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: MaterialApp(
-        home: Text('Hola Mundo'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hola Mundo'),
+        ),
       ),
     );
   }
